557acc2360b6e20ab44806b6d52a3fb65919b9fd
냠냠 <lanilani7661@gmail.com>
2022-09-29 19:02:36 +0900

    플레이어 베이스 요소추가

--- Assets/Scripts/Base/User.cs
+++ Assets/Scripts/Base/User.cs
@@ -4,4 +4,10 @@ using System.Collections.Generic;
 public class User
 {
     public int money;
+    public float attack;                // 공격력
+    public float attackSpeed;           // 공격속도
+    public float defense;               // 방어력
+    public float criticalChance;        // 치명타 확률
+    public float criticalDamage;        // 치명타 데미지
+    public int maxMagazine;             // 최대 탄창
 }
