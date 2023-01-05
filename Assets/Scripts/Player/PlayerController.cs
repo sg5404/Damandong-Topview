3e2c6e049e25fb9b757bfeb1546dea4318241a6f
rlagmltn <gmltn2015@naver.com>
2023-01-05 19:59:40 +0900

    바꿔봄

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -322,7 +322,7 @@ public class PlayerController : MonoSingleton<PlayerController>
                     }
                 }
                 Debug.Log((SoundManager.STATE)(weaponSet.MainWeaponState - 1));
-                SoundManager.Instance.EffectSoundPlay((SoundManager.STATE)weaponSet.MainWeaponState - 1);
+                SoundManager.Instance.EffectSoundPlay((SoundManager.STATE)weaponSet.SubWeaponState - 1);
                 showFireEff(0);
                 leftCurtime = 0;
                 leftTimer = 0.08f;
@@ -367,7 +367,7 @@ public class PlayerController : MonoSingleton<PlayerController>
                         bullet.transform.SetParent(null);
                     }
                 }
-                Debug.Log((SoundManager.STATE)(weaponSet.SubWeaponState - 1));
+                Debug.Log((SoundManager.STATE)(weaponSet.MainWeaponState - 1));
                 SoundManager.Instance.EffectSoundPlay((SoundManager.STATE)(weaponSet.SubWeaponState - 1));
 
                 rightCurtime = 0;
